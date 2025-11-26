95e22afb3d12ab22b3bf5559a2cb4a2d836f57d0
A-M-Haadi <aal3it@gmail.com>
2025-11-26 22:01:25 +0800

    feat: menambahkan admin seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+            'store_status' => null
+        ]);
+
+    }
+}
