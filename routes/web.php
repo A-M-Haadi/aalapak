515dd380d6772738216ce0bf5808db0ab0ffa220
A-M-Haadi <aal3it@gmail.com>
2025-11-26 22:35:17 +0800

    feat: menambahkan rute untuk halaman verifikasi

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,26 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::prefix('admin')->name('admin.')->group(function () {
-        Route::resource('categories', CategoryController::class);
-        Route::get('/sellers', [SellerVerificationController::class, 'index'])->name('sellers.index');
-        Route::patch('/sellers/{user}', [SellerVerificationController::class, 'update'])->name('sellers.update');
-    });
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    
+    Route::prefix('admin')->name('admin.')->group(function () {
+
+        Route::resource('categories', CategoryController::class);
+
+        Route::get('/sellers', [SellerVerificationController::class, 'index'])->name('sellers.index');
+        Route::patch('/sellers/{user}', [SellerVerificationController::class, 'update'])->name('sellers.update');
+        Route::resource('users', UserController::class)->only([
+                'index', 'edit', 'update', 'destroy'
+            ]);
+        Route::get('/products', [ProductManagementController::class, 'index'])->name('products.index');
+        Route::delete('/products/{product}', [ProductManagementController::class, 'destroy'])->name('products.destroy');
+    });
+
+});
