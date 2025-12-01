a41708d71e5397c29b2c42baee811b0fa1be2fb2
A-M-Haadi <aal3it@gmail.com>
2025-12-01 12:32:19 +0800

    feat: membuat database untuk review

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -33,4 +33,9 @@ class Product extends Model
     {
         return $this->hasMany(CartItem::class);
     }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
